53f1ab9c0b0ca78c6bda15052da5489cf0806562
abshrimp <abshrimps@gmail.com>
2026-03-02 10:49:11 +0900

    a

--- stylegan2-ada-pytorch/generate.py
+++ stylegan2-ada-pytorch/generate.py
@@ -79,7 +79,7 @@ def generate_images(
     """
 
     print('Loading networks from "%s"...' % network_pkl)
-    device = torch.device('cuda')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     with dnnlib.util.open_url(network_pkl) as f:
         G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
 
